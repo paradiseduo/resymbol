6b842c55aa08a9b3cf92461382aa28770f484fbd
paradiseduo <>
2022-01-10 11:40:11 +0800

    新增编译脚本

---
+++ build-macOS_x86.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+NAME=resymbol
+
+function build() {
+  START=$(date +%s)
+
+  swift build --product $NAME \
+    -c release \
+    -Xswiftc "-sdk" \
+    -Xswiftc "$(xcrun --sdk macosx --show-sdk-path)" \
+    -Xswiftc "-target" \
+    -Xswiftc "x86_64-apple-macosx10.13" \
+    -Xcc "-arch" \
+    -Xcc "x86_64" \
+    -Xcc "--target=x86_64-apple-macosx10.13" \
+    -Xcc "-isysroot" \
+    -Xcc "$(xcrun --sdk macosx --show-sdk-path)"
+
+  END=$(date +%s)
+  TIME=$(($END - $START))
+  echo "build in $TIME seconds"
+}
+
+function copy() {
+ cp .build/release/resymbol ./resymbol
+}
+
+function main() {
+  build
+  copy
+}
+
+main
+
+
