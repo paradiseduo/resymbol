a4857b7a163274bf3a9158ae72fcdbfc36b564e7
paradiseduo <>
2022-02-17 16:21:27 +0800

    新增arm处理器的编译脚本

---
+++ build-macOS_arm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+NAME=resymbol
+
+function build() {
+  START=$(date +%s)
+
+  swift build --product $NAME \
+    -c release \
+    -Xswiftc "-sdk" \
+    -Xswiftc "$(xcrun --sdk macosx --show-sdk-path)" \
+    -Xswiftc "-target" \
+    -Xswiftc "arm64-apple-macosx11.0" \
+    -Xcc "-arch" \
+    -Xcc "arm64" \
+    -Xcc "--target=arm64-apple-macosx11.0" \
+    -Xcc "-isysroot" \
+    -Xcc "$(xcrun --sdk macosx --show-sdk-path)"
+
+  END=$(date +%s)
+  TIME=$(($END - $START))
+  echo "build in $TIME seconds"
+}
+
+function copy() {
+ cp .build/release/resymbol ./resymbol
+}
+
+function main() {
+  build
+  copy
+}
+
+main
+
+
